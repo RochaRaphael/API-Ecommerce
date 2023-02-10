3b549a8d89aa530583015a13b9b30ab02fc70f75
Raphael Rocha <rocharaphael0911@outlook.com>
2023-02-10 14:56:12 -0300

    Category Model added

---
+++ API-Ecommerce/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace API_Ecommerce.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public bool Active { get; set; }
+        public bool Deleted { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
