aed1ed5fa9219d15d6b1b9891bcf62f3da3c612b
Raphael Rocha <rocharaphael0911@outlook.com>
2023-02-10 14:53:34 -0300

    Order Model added

---
+++ API-Ecommerce/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace API_Ecommerce.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public List<Product> Products { get; set; }
+
+    }
+}
