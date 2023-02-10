fae9cc451761a273978ef58d30e686482f10f302
Raphael Rocha <rocharaphael0911@outlook.com>
2023-02-10 14:25:32 -0300

    User Model added

--- API-Ecommerce/Models/User.cs
+++ API-Ecommerce/Models/User.cs
@@ -2,5 +2,17 @@
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string VerificationKey { get; set; }
+        public string LastToken { get; set; }
+        public bool IsChecked { get; set; }
+        public bool Active { get; set; }
+        public bool Excluded { get; set; }
+
+        public List<Order> Orders { get; set; }
     }
 }
