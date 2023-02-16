f9605bfe80bf9a138cde2996a4f32b642fe10764
Raphael Rocha <rocharaphael0911@outlook.com>
2023-02-16 12:01:22 -0300

    Mapping category table

---
+++ API-Ecommerce/Data/Mapping/CategoryMap.cs
@@ -0,0 +1,42 @@
+﻿using API_Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API_Ecommerce.Data.Mapping
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(256);
+
+            builder.Property(x => x.Url)
+                .IsRequired()
+                .HasColumnName("Url")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(256);
+
+            builder.Property(x => x.Active)
+                .IsRequired()
+                .HasColumnName("Active")
+                .HasColumnType("BIT");
+
+            builder.Property(x => x.Deleted)
+                .IsRequired()
+                .HasColumnName("Deleted")
+                .HasColumnType("BIT");
+        }
+    }
+}
