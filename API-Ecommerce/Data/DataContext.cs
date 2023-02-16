06101edcef5e69de36a5744f1f346109774809bf
Raphael Rocha <rocharaphael0911@outlook.com>
2023-02-16 11:40:56 -0300

    Creatting DataContext

--- API-Ecommerce/Data/DataContext.cs
+++ API-Ecommerce/Data/DataContext.cs
@@ -1,6 +1,19 @@
-﻿namespace API_Ecommerce.Data
+﻿using API_Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Ecommerce.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> Usuarios { get; set; }
+        public DbSet<Product> Produtos { get; set; }
+        public DbSet<Order> Pedidos { get; set; }
+        public DbSet<Category> Categorias { get; set; }
+
+        
     }
 }
