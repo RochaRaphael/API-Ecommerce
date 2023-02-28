089e99150e82e2fb8a502322e84c753e9c393b15
Raphael Rocha <rocharaphael0911@outlook.com>
2023-02-28 15:37:44 -0300

    Adding HomeController

---
+++ API-Ecommerce/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Ecommerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Get(
+            [FromServices] IConfiguration config)
+        {
+            var env = config.GetValue<string>("Env");
+            return Ok(new
+            {
+                environment = env
+            });
+        }
+    }
+}
