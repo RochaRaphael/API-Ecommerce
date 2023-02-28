90deda7f693d61fc6a147a07a5cf3ddc03e98319
Raphael Rocha <rocharaphael0911@outlook.com>
2023-02-28 14:08:36 -0300

    Configuring services

--- API-Ecommerce/Program.cs
+++ API-Ecommerce/Program.cs
@@ -1,6 +1,23 @@
+using API_Ecommerce.Data;
+using API_Ecommerce.Repositories;
+using API_Ecommerce.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services.AddDbContext<DataContext>(
+        options =>
+            options.UseSqlServer(connectionString));
+
+    builder.Services.AddScoped<UserRepositories>();
+    builder.Services.AddScoped<UserServices>();
+
+}
