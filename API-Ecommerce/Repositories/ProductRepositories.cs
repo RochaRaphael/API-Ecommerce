311bdac9adbe9a40b0dd47b891071d8487b6e985
Raphael Rocha <rocharaphael0911@outlook.com>
2023-03-02 14:41:26 -0300

    Added repository, service, controller for product

---
+++ API-Ecommerce/Repositories/ProductRepositories.cs
@@ -0,0 +1,6 @@
+﻿namespace API_Ecommerce.Repositories
+{
+    public class ProductRepositories
+    {
+    }
+}
---
+++ API-Ecommerce/Services/ProductServices.cs
@@ -0,0 +1,6 @@
+﻿namespace API_Ecommerce.Services
+{
+    public class ProductServices
+    {
+    }
+}
