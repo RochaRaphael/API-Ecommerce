6b24677c81b69444545bff8311de4b0c516fe496
Raphael Rocha <rocharaphael0911@outlook.com>
2023-03-03 14:13:30 -0300

    Added new ViewModel

---
+++ API-Ecommerce/ViewModels/ShowProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿using API_Ecommerce.Models;
+
+namespace API_Ecommerce.ViewModels
+{
+    public class ShowProductViewModel
+    {
+        public Category Category { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public int Quantity { get; set; }
+    }
+}
