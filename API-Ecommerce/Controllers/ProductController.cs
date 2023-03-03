05491877b3df948b440470242f33faad97b53930
Raphael Rocha <rocharaphael0911@outlook.com>
2023-03-03 14:20:57 -0300

    Added GetByIdAsync in ProductController

--- API-Ecommerce/Controllers/ProductController.cs
+++ API-Ecommerce/Controllers/ProductController.cs
@@ -1,6 +1,40 @@
-﻿namespace API_Ecommerce.Controllers
+﻿using API_Ecommerce.Extensions;
+using API_Ecommerce.Models;
+using API_Ecommerce.Services;
+using API_Ecommerce.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Ecommerce.Controllers
 {
-    public class ProductController
+    [ApiController]
+    public class ProductController : ControllerBase
     {
+        private readonly ProductServices productServices;
+        public ProductController(ProductServices productService)
+        {
+            this.productServices = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetById(
+            [FromRoute] int id
+            )
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+
+            try
+            {
+                var product = await productServices.GetByIdAsync(id);
+                if (product == null)
+                    return NotFound("Product not found");
+
+                return Ok(new ResultViewModel<Product>(product));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<string>("42X00 - Server failure"));
+            }
+        }
     }
 }
