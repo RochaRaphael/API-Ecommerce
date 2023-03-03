de13f2fa8f4b2488e9acf7a29ff3ecafc17b4d47
Raphael Rocha <rocharaphael0911@outlook.com>
2023-03-03 15:47:20 -0300

    Updated ProductServices and CategoryServices

---
+++ API-Ecommerce/Services/CategoryServices.cs
@@ -0,0 +1,56 @@
+﻿using API_Ecommerce.Models;
+using API_Ecommerce.Repositories;
+using API_Ecommerce.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Ecommerce.Services
+{
+    public class CategoryServices
+    {
+        private readonly CategoryRepositories categoryRepositories;
+        public CategoryServices(CategoryRepositories categoryRepositories)
+        {
+            this.categoryRepositories = categoryRepositories;
+        }
+
+        public async Task<Category> GetByIdAsync(int id)
+        {
+            try
+            {
+                var category = await categoryRepositories.GetByIdAsync(id);
+                if (category == null)
+                    return null;
+
+                return category;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+        public async Task<ResponseViewModel> RegisterCategoryAsync(string name)
+        {
+            try
+            {
+                if (await categoryRepositories.CategoryExistsAsync(name))
+                    return new ResponseViewModel { Success = false, Message = "This category already exists" };
+
+                var newCategory = new Category
+                {
+                    Name = name,
+                    Url = $"/category/{name}/",
+                    Active = true,
+                    Deleted = false
+                };
+                await categoryRepositories.RegisterCategoryAsync(newCategory);
+                return new ResponseViewModel { Success = true, Message = "User successfully created" };
+            }
+            catch (DbUpdateException)
+            {
+                return new ResponseViewModel { Success = false, Message = "44X85 - Server failure" };
+            }
+        }
+    }
+}
--- API-Ecommerce/Services/ProductServices.cs
+++ API-Ecommerce/Services/ProductServices.cs
@@ -1,6 +1,66 @@
-﻿namespace API_Ecommerce.Services
+﻿using API_Ecommerce.Models;
+using API_Ecommerce.Repositories;
+using API_Ecommerce.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Ecommerce.Services
 {
     public class ProductServices
     {
+        private readonly ProductRepositories productRepositories;
+        private readonly CategoryRepositories categoryRepositories;
+        private readonly CategoryServices categoryServices;
+        public ProductServices(ProductRepositories productRepositories, CategoryRepositories categoryRepositories, CategoryServices categoryServices)
+        {
+            this.productRepositories = productRepositories;
+            this.categoryRepositories = categoryRepositories;
+            this.categoryServices = categoryServices;
+        }
+
+
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            try
+            {
+                var user = await productRepositories.GetByIdAsync(id);
+                if (user == null)
+                    return null;
+
+                return user;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<ResponseViewModel> RegisterProductAsync(NewProductViewModel product)
+        {
+            try
+            {
+                var productCategory = await categoryRepositories.GetByNameAsync(product.Category);
+                if (productCategory == null)
+                {
+                    await categoryServices.RegisterCategoryAsync(product.Category);
+                    productCategory = await categoryRepositories.GetByNameAsync(product.Category);
+                }
+                    
+                var newProduct = new Product
+                {
+                    Name = product.Name,
+                    Url = $"/product/{product.Name}/",
+                    Quantity = product.Quantity,
+                    Active = true,
+                    Deleted = false
+                };
+                await productRepositories.RegisterProductAsync(newProduct);
+                return new ResponseViewModel { Success = true, Message = "Product successfully created" };
+            }
+            catch (DbUpdateException)
+            {
+                return new ResponseViewModel { Success = false, Message = "32X88 - Server failure" };
+            }
+        }
     }
 }
