e6ff194beeffb933252ad4c3ff911f156b6ce0d3
Raphael Rocha <rocharaphael0911@outlook.com>
2023-03-03 15:48:52 -0300

    Updated ProductRepositories and CategoryRepositories

---
+++ API-Ecommerce/Repositories/CategoryRepositories.cs
@@ -0,0 +1,39 @@
+﻿using API_Ecommerce.Data;
+using API_Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Ecommerce.Repositories
+{
+    public class CategoryRepositories
+    {
+        private readonly DataContext context;
+
+        public CategoryRepositories(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Category> GetByIdAsync(int id)
+        {
+            return await context
+                .Categories
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task RegisterCategoryAsync(Category newCategory)
+        {
+            await context.Categories.AddAsync(newCategory);
+            await context.SaveChangesAsync();
+        }
+        public async Task<bool> CategoryExistsAsync(string name)
+        {
+            return await Task.FromResult(context.Categories.Any(x => x.Name == name));
+        }
+        public async Task<Category> GetByNameAsync(string name)
+        {
+            return await context
+                .Categories
+                .FirstOrDefaultAsync(x => x.Name == name);
+        }
+    }
+}
