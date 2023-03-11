f382e98f66beb29095e7cee111eabebda01dbf35
Raphael Rocha <rocharaphael0911@outlook.com>
2023-03-11 20:22:13 -0300

    Config Token JWT

--- API-Ecommerce/Program.cs
+++ API-Ecommerce/Program.cs
@@ -1,18 +1,51 @@
+using API_Ecommerce;
 using API_Ecommerce.Data;
 using API_Ecommerce.Repositories;
 using API_Ecommerce.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigureMvc(builder);
 ConfigureServices(builder);
 
 builder.Services.AddEndpointsApiExplorer();
-var app = builder.Build();
 
+var app = builder.Build();
+LoadConfiguration(app);
+
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void LoadConfiguration(WebApplication app)
+{
+    Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
+}
+
+void ConfigureAuthentication(WebApplicationBuilder builder)
+{
+    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+    builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+}
 
 void ConfigureMvc(WebApplicationBuilder builder)
 {
@@ -20,7 +53,12 @@ void ConfigureMvc(WebApplicationBuilder builder)
     .ConfigureApiBehaviorOptions(options =>
     {
         options.SuppressModelStateInvalidFilter = true;
-    });
+    })
+    .AddJsonOptions(x =>
+    {
+        x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+    }); ;
 }
 
 void ConfigureServices(WebApplicationBuilder builder)
@@ -29,6 +67,7 @@ void ConfigureServices(WebApplicationBuilder builder)
     builder.Services.AddDbContext<DataContext>(
         options =>
             options.UseSqlServer(connectionString));
+    builder.Services.AddTransient<TokenService>();
 
     builder.Services.AddScoped<UserRepositories>();
     builder.Services.AddScoped<UserServices>();
@@ -36,5 +75,7 @@ void ConfigureServices(WebApplicationBuilder builder)
     builder.Services.AddScoped<CategoryServices>();
     builder.Services.AddScoped<ProductRepositories>();
     builder.Services.AddScoped<ProductServices>();
+    builder.Services.AddScoped<RoleRepositories>();
+    builder.Services.AddScoped<RoleServices>();
 
 }
