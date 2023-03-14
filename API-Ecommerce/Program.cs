72e5559c9158e6439678e18aad8088ceae7a0f95
Raphael Rocha <rocharaphael0911@outlook.com>
2023-03-14 15:19:09 -0300

    Added ConfigureAuthentication

--- API-Ecommerce/Program.cs
+++ API-Ecommerce/Program.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+ConfigureAuthentication(builder);
 ConfigureMvc(builder);
 ConfigureServices(builder);
 
