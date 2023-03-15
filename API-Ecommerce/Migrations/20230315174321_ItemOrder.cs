0f41d25cb161f9ea31c5f1b0f94abcfebd789e39
Raphael Rocha <rocharaphael0911@outlook.com>
2023-03-15 14:57:38 -0300

    New Migrations

---
+++ API-Ecommerce/Migrations/20230315174321_ItemOrder.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class ItemOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PedidoItem");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Order",
+                type: "DATE",
+                nullable: false,
+                defaultValue: new DateTime(2023, 3, 15, 17, 43, 20, 886, DateTimeKind.Utc).AddTicks(7534),
+                oldClrType: typeof(DateTime),
+                oldType: "DATE",
+                oldDefaultValue: new DateTime(2023, 3, 11, 16, 55, 35, 553, DateTimeKind.Utc).AddTicks(1245));
+
+            migrationBuilder.CreateTable(
+                name: "ItemOrder",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemOrder", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItemOrder_Order",
+                        column: x => x.OrderId,
+                        principalTable: "Order",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemOrder_Product",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemOrder_OrderId",
+                table: "ItemOrder",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemOrder_ProductId",
+                table: "ItemOrder",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemOrder");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Order",
+                type: "DATE",
+                nullable: false,
+                defaultValue: new DateTime(2023, 3, 11, 16, 55, 35, 553, DateTimeKind.Utc).AddTicks(1245),
+                oldClrType: typeof(DateTime),
+                oldType: "DATE",
+                oldDefaultValue: new DateTime(2023, 3, 15, 17, 43, 20, 886, DateTimeKind.Utc).AddTicks(7534));
+
+            migrationBuilder.CreateTable(
+                name: "PedidoItem",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PedidoItem", x => new { x.OrderId, x.ProductId });
+                    table.ForeignKey(
+                        name: "FK_Order_ProductId",
+                        column: x => x.OrderId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Product_OrderId",
+                        column: x => x.ProductId,
+                        principalTable: "Order",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidoItem_ProductId",
+                table: "PedidoItem",
+                column: "ProductId");
+        }
+    }
+}
